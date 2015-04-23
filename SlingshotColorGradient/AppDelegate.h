8dcffcbf40cc8336267d4e6e54963985687aecab
n6xej <cworley@Christophers-MacBook-Pro.local>
2015-04-23 11:33:43 -0600

    Initial Commit

---
+++ SlingshotColorGradient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlingshotColorGradient
+//
+//  Created by Christopher Worley on 4/23/15.
+//  Copyright (c) 2015 Christopher Worley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlingshotColorGradient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlingshotColorGradient
+//
+//  Created by Christopher Worley on 4/23/15.
+//  Copyright (c) 2015 Christopher Worley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
